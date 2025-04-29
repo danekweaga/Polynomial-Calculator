9cb8e54ba88d9d0f58b02bb9f5178840bfec65b9
Chukwunonso Daniel Ekweaga <88860622+danekweaga@users.noreply.github.com>
2025-04-29 20:59:06 -0300

    Update PolynomialCalculatorApp.java

--- v1/PolynomialCalculatorApp.java
+++ v1/PolynomialCalculatorApp.java
@@ -1,24 +1,29 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
+/**********************************************************************************************
+ * @title PolynomialCalculatorApp
+
+ * @author Chukwunonso Daniel Ekweaga
+ **********************************************************************************************/
 public class PolynomialCalculatorApp extends Application 
 {
     @Override
     public void start(Stage primaryStage) 
     {
+        // Create an instance of the GUI
+        primaryStage.initStyle(StageStyle.UNDECORATED);
         PolynomialCalculatorGUI gui = new PolynomialCalculatorGUI();
+        
+        // Build the GUI and attach it to the primary stage
         Scene scene = gui.buildGUI(primaryStage);
-
+        
+        // Set up the stage
         primaryStage.setScene(scene);
         primaryStage.setTitle("Polynomial Calculator");
         primaryStage.setResizable(false);
         primaryStage.show();
     }
-    
-    public static void main(String[] args) 
-    {
-        launch(args);
-    }
-
 }
